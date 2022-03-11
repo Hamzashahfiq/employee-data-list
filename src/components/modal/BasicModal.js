be73d4247cbc64b5ebd3ac2c9ac9be7423c806dc
Muhammad Hamza <hamzashafique535@gmail.com>
2022-03-11 17:50:01 +0500

    compelte CRUD operation app of emplyee data

--- src/components/modal/BasicModal.js
+++ src/components/modal/BasicModal.js
@@ -10,6 +10,7 @@ import Header from '../header/Header'
 import ButtonBasic from "../button/ButtonBasic"
 import TextField from '@mui/material/TextField';
 import { textAlign } from '@mui/system';
+import { red } from '@mui/material/colors';
 
 
 const style = {
@@ -23,12 +24,12 @@ const style = {
   boxShadow: 24,
 };
 
-export default function BasicModal({ resetFormData,updateBtnFlageProp, viewBtnFlageProp,modelopen,modelState, modelclose,submitHandler, formDataValue, setStateFirstName, setStateLastName, setStateEmail, setStatePhoneNo }) {
+
+export default function BasicModal({ resetFormData, updateBtnFlageProp, updateCtaHandlerProps, viewBtnFlageProp, modelopen, modelState, modelclose, submitHandler, formDataValue, setStateFirstName, setStateLastName, setStateEmail, setStatePhoneNo }) {
   // const [open, setOpen] = React.useState(false);
   // const handleOpen = () => setOpen(true);
   // const handleClose = () => setOpen(false);
   // console.log(modelFlageProp)
-
 
   return (
     <div>
@@ -53,22 +54,35 @@ export default function BasicModal({ resetFormData,updateBtnFlageProp, viewBtnFl
                 placeholder={item.placeholder} inputChange = {setStateData} inputValue= {formDataValue} />
               )
             })} */}
-              <Box sx={{ p: 1, textAlign: 'center', width: { xs: 'auto', md: '584px' } }}>
-                <TextField type='text' id="outlined-basic" sx={{ m: 1, width: { xs: 'auto', md: '260px' } }} onChange={setStateFirstName}
-                  value={formDataValue.firstName} label='First Name' placeholder='First Name' variant="outlined" required />
-                <TextField type='text' id="outlined-basic" onChange={setStateLastName} sx={{ m: 1, width: { xs: 'auto', md: '260px' } }}
-                  value={formDataValue.lastName} label='Last Name' placeholder='Last Name' variant="outlined" required />
-                <TextField type='email' id="outlined-basic" onChange={setStateEmail} sx={{ m: 1, width: { xs: 'auto', md: '260px' } }}
-                  value={formDataValue.email} label='Email' placeholder='Email' variant="outlined" required />
-                <TextField type='number' id="outlined-basic" onChange={setStatePhoneNo} sx={{ m: 1, width: { xs: 'auto', md: '260px' } }}
-                  value={formDataValue.phoneNo} label='Phone No' placeholder='Phone No' variant="outlined" />
-              </Box>
+              {viewBtnFlageProp ?
+                <Box sx={{ p: 1, textAlign: 'center', width: { xs: 'auto', md: '584px' } }}>
+                  <TextField type='text' id="outlined-basic" sx={{ m: 1, width: { xs: 'auto', md: '260px' } }} onChange={setStateFirstName}
+                    value={formDataValue.firstName} label='First Name' placeholder='First Name' variant="outlined" required />
+                  <TextField type='text' id="outlined-basic" onChange={setStateLastName} sx={{ m: 1, width: { xs: 'auto', md: '260px' } }}
+                    value={formDataValue.lastName} label='Last Name' placeholder='Last Name' variant="outlined" required />
+                  <TextField type='email' id="outlined-basic" onChange={setStateEmail} sx={{ m: 1, width: { xs: 'auto', md: '260px' } }}
+                    value={formDataValue.email} label='Email' placeholder='Email' variant="outlined" required />
+                  <TextField type='number' id="outlined-basic" onChange={setStatePhoneNo} sx={{ m: 1, width: { xs: 'auto', md: '260px' } }}
+                    value={formDataValue.phoneNo} label='Phone No' placeholder='Phone No' variant="outlined" />
+                </Box> :
+                <Box sx={{ p: 1, textAlign: 'center', width: { xs: 'auto', md: '584px' } }}>
+                  <TextField type='text' id="outlined-basic" sx={{ m: 1, width: { xs: 'auto', md: '260px' } }} onChange={setStateFirstName}
+                    value={formDataValue.firstName} label='First Name' placeholder='First Name' variant="outlined" required    InputProps={{ readOnly: true,}}/>
+                  <TextField type='text' id="outlined-basic" onChange={setStateLastName} sx={{ m: 1, width: { xs: 'auto', md: '260px' } }}
+                    value={formDataValue.lastName} label='Last Name' placeholder='Last Name' variant="outlined" required InputProps={{ readOnly: true,}}/>
+                  <TextField type='email' id="outlined-basic" onChange={setStateEmail} sx={{ m: 1, width: { xs: 'auto', md: '260px' } }}
+                    value={formDataValue.email} label='Email' placeholder='Email' variant="outlined" required InputProps={{ readOnly: true,}}/>
+                  <TextField type='number' id="outlined-basic" onChange={setStatePhoneNo} sx={{ m: 1, width: { xs: 'auto', md: '260px' } }}
+                    value={formDataValue.phoneNo} label='Phone No' placeholder='Phone No' variant="outlined" InputProps={{ readOnly: true,}} />
+                </Box>
+              }
 
-              {viewBtnFlageProp?
-              <Box sx={{ p: 1, textAlign: 'center', width: { xs: 'auto', md: '584px' } }}>
-              {updateBtnFlageProp?<Button onClick={submitHandler} variant="contained" style={{ textTransform: 'capitalize', backgroundColor: '#002984', margin: '8px', width: '80px' }}>Update</Button>:<Button onClick={submitHandler} variant="contained" style={{ textTransform: 'capitalize', backgroundColor: '#002984', margin: '8px', width: '80px' }}>Submit</Button>}
-                <Button onClick={resetFormData} variant="contained" style={{ textTransform: 'capitalize', backgroundColor: '#ff6f00', margin: '8px', width: '80px' }}>Clear</Button>
-              </Box>: null}
+
+              {viewBtnFlageProp ?
+                <Box sx={{ p: 1, textAlign: 'center', width: { xs: 'auto', md: '584px' } }}>
+                  {updateBtnFlageProp ? <Button onClick={updateCtaHandlerProps} variant="contained" style={{ textTransform: 'capitalize', backgroundColor: '#002984', margin: '8px', width: '80px' }}>Update</Button> : <Button onClick={submitHandler} variant="contained" style={{ textTransform: 'capitalize', backgroundColor: '#002984', margin: '8px', width: '80px' }}>Submit</Button>}
+                  {updateBtnFlageProp ? null : <Button onClick={resetFormData} variant="contained" style={{ textTransform: 'capitalize', backgroundColor: '#ff6f00', margin: '8px', width: '80px' }}>Clear</Button>}
+                </Box> : null}
             </Box>
           </form>
 
